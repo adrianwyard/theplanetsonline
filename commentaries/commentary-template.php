78d7a18049a0aa65c860c4b00baf6b5670fec729
Candice <kaye_candy14@yahoo.com>
2018-10-23 13:43:25 +0800

    Options bugs

--- commentaries/commentary-template.php
+++ commentaries/commentary-template.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 
-	<commentary id="welcome-commentary" type="com0" time="00:00:00" iframe="./pages/welcome.html">
+	<commentary id="welcome-commentary" type="com0" time="00:00:00">
 	
 		<h1 class="com0"><a tref="00:00:00">00:00</a>&nbsp;<a href="../pages/welcome.html">Welcome to The Planets Online</a></h1>
 		<p>Viewing <span class="commentary-type-active"></span> of <span class="commentary-type-total"></span> <a class="cndce-open-options">Comment Channels</a>: <span class="commentary-type-tags"></span></p>
