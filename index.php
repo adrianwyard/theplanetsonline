001549528ee5ad31d96ee543e18fb3d6e8b24093
Candice <kaye_candy14@yahoo.com>
2018-12-10 14:15:56 +0800

    Bug Fix: hide #aw-all-links

--- index.php
+++ index.php
@@ -41,11 +41,9 @@
 
 	<div id="cndce-container" class="cndce-no-tabs">
 		<!-- AW explicitly list all available content and links for search crawlers -->
-		<div id="aw-all-links">
+		<div id="aw-all-links" style="display: none">
 			<?php include('./commentaries/all-planets.html') ?>
 		</div>
-		<!-- don't show content that's duplicated for crawlers on load -->
-		<script>document.getElementById('aw-all-links').style.display = 'none';</script>
 		<!-- AW End -->
 
 		<div id="cndce-options-container">
