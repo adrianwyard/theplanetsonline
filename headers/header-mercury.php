b35a66ce6fd34d0e6164233e89b2bee9e4974961
Candice <kaye_candy14@yahoo.com>
2019-01-25 03:22:33 +0800

    SEO FIX: Updated canonical links for the planets

--- headers/header-mercury.php
+++ headers/header-mercury.php
@@ -1,3 +1,3 @@
 	<title>The Planets Online - Mercury</title>
 	<meta name="description" content="Learn about Mercury - science, music, trivia, and much more - all while accompanied by an orchestra">
-	<link rel="canonical" href="https://theplanetsonline.com/mercury.php">
+	<link rel="canonical" href="https://theplanetsonline.com/mercury">
--- headers/header-saturn.php
+++ headers/header-saturn.php
@@ -1,3 +1,3 @@
 	<title>The Planets Online - Saturn</title>
 	<meta name="description" content="Learn about Saturn - science, music, trivia, and much more - all while accompanied by an orchestra">
-	<link rel="canonical" href="https://theplanetsonline.com/saturn.php">
+	<link rel="canonical" href="https://theplanetsonline.com/saturn">
